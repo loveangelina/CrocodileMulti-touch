f099ef7e63abcf689fae14d6dfb8d76a7c03917c
loveangelina <90149613+loveangelina@users.noreply.github.com>
2023-10-27 20:27:35 +0900

    initial commit
    add crocodile, water asset

---
+++ Assets/Scripts/Main/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance => instance;
+
+    private void Awake()
+    {
+        // 인스턴스가 이미 존재하는 경우, 새로 생기는 인스턴스 삭제
+        if (instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        // 인스턴스를 유일 오브젝트로 만듦
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
