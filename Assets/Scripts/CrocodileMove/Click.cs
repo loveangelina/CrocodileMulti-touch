0df5a9a4f1ff5abc759476ff32e749b1725461a7
leejinwoo26 <142082239+leejinwoo26@users.noreply.github.com>
2023-11-01 13:20:45 +0900

    add crocodile Attackmotion and coding
    마우스포인터를 이용한 악어움직임 및 악어 공격 애니메이션 실행

---
+++ Assets/Scripts/CrocodileMove/Click.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    public float rotateSpeed = 10f;
+
+    public Vector3 destinationPoint;
+
+    public float waitTime = 2f;
+    public bool ShoudMove = false;
+    public bool ShouldAttack = false;
+    private Look look;
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        look = GetComponent<Look>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //마우스 왼쪽 버튼 클릭시
+        {
+            
+            look.IsTouch = true; // 손가락 터치가 true
+
+            look.IsMove = true;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                destinationPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z); //포인트의 위치를  hit포인트로 새로 지정, y축은 악어의 위치로
+
+                ShoudMove = true; // 움직임이 true 로 바뀜
+            }
+        }
+        if (ShoudMove) // 움직임이 true 일때 실행
+        {
+            //이동 애니메이션 재생
+            animator.SetBool("Sprint", true);
+            //공격 애니메이션 멈춤
+            animator.SetBool("Attack", false);
+            //악어가 보아야할 방향을 targetRotation 로 지정
+            Quaternion targetRotation = Quaternion.LookRotation(destinationPoint - transform.position);
+            //회전을 스무스 하게 targetRotation 방향으로 함 
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed*Time.deltaTime);
+            //움직임은 포인트의 위치로 이동
+            transform.position = Vector3.MoveTowards(transform.position, destinationPoint, moveSpeed*Time.deltaTime);
+            //공격 비활성화
+            ShouldAttack = false;
+            //포인트의 위치가 0.2f보다 가까워 지면 
+            if (Vector3.Distance(transform.position,destinationPoint)<= 0.2f)
+            {
+                ShoudMove = false; //움직임 멈춤
+                animator.SetBool("Sprint", false);//이동 애니메이션 멈춤
+                ShouldAttack = true;//공격 가능              
+            }
+            if (ShouldAttack)
+            {               
+                animator.SetBool("Attack", true);//공격 애니메이션 재생          
+            }
+        }
+    }
+}
--- Assets/Scripts/CrocodileMove/Look.cs
+++ Assets/Scripts/CrocodileMove/Look.cs
@@ -13,14 +13,22 @@ public class Look : MonoBehaviour
     public float rotate = 0.5f;
     public float attackRotate = 0.5f;
     public float respon = 5f;
-    public Transform[] moveSpot;
     
+    public Transform[] moveSpot;
+
+    public Vector3 destPos;
+    public Vector3 dir;
+    public Quaternion lookTarget;
+
     private int randomSpot;
     public bool IsTouch;
     public bool IsMove;
     Animator animator;
 
-    public List<Vector3> attackSpots = new List<Vector3>();
+    public Vector3 mousePosition;
+    public Vector3 worldPosition;
+
+
     private void Start()
     {
         waitTime = startWaitTime;
@@ -28,87 +36,45 @@ public class Look : MonoBehaviour
         animator = GetComponent<Animator>();
         IsTouch = false;
         IsMove = false;
-        
-
     }
     private void Update()
     {
-        
-        if(!IsTouch) //터치안할때
+
+        if (!IsTouch) //터치안할때
         {
-            if(!IsMove)//움직이지 않을때
+            if (!IsMove)//움직이지 않을때
             {
                 Move(); //움직임 활성화
                 animator.SetBool("Sprint", true); // 애니메이션 움직임 활성화
-               
+
                 if (Vector3.Distance(transform.position, moveSpot[randomSpot].position) <= 0.2f) // 무브스팟과의 거리가 0.1f 보다 가까워지면
                 {
                     //시간을감소, 웨이트 타임이 0보다 작아지면 다시 무빙상태 (그전은 아이들 상태)
 
-                    
+
                     if (waitTime <= 0)
                     {
-                        Debug.Log("이동활성화");
+                        //Debug.Log("이동활성화");
                         randomSpotMaking();//랜덤으로 가야할곳 설정
                         IsMove = false; //움직임 활성화
                         waitTime = Random.Range(0, 2f);
-                        Debug.Log(waitTime);
+                        //Debug.Log(waitTime);
                         startWaitTime = waitTime;
                     }
                     else
                     {
                         //움직임 비활성화
-                        Debug.Log("이동 비활성화");
+                        //Debug.Log("이동 비활성화");
                         animator.SetBool("Sprint", false);
                         waitTime -= Time.deltaTime;
                     }
-
-
-                   
-                }
-                else //무브스팟의 거리가 0.1 보다 멀면
-                {
-                    // 무빙상태유지
-                    
-                }
-            }
-            else //움직일때
-            {
-                //애니메이션 아이들 활성화
-                //움직임 x
-            }
+                }               
+            }          
         }
-        if (Input.GetMouseButtonDown(0)) //마우스 왼쪽 버튼을 누르면
-        {
-
-            //누른 위치 구하기
-            Vector3 AttackPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-            //누른 위치 attackSpot 리스트에 삽입
-            
-            attackSpots.Add(AttackPoint);
-            Debug.Log(attackSpots);
-
-            //누른 위치로 악어가 이동
-
-            //transform.position = Vector3.MoveTowards(transform.position, new Vector3(AttackPoint.x, 0, AttackPoint.z), speed * Time.deltaTime);
-            //누른 위치로 악어가 부드럽게 회전
-            //Quaternion AttackAngle = Quaternion.LookRotation(new Vector3(AttackPoint.x,transform.position.y,AttackPoint.z));
-            //transform.rotation = Quaternion.Slerp(transform.rotation,AttackAngle, attackRotate * Time.deltaTime);
-            //악어가 위치로 가까이 이동하면 공격 애니메이션 재생
-           
-            //위치 포인트 로그
-            Debug.Log(AttackPoint.ToString());
-        }
-
-        
-        else //터치 할때
-        {
-            //터치한 곳 위치를 받아서 저장한후 저장한곳으로 무빙
-        }
-
     }
     private void Move() //움직임과 회전을 함
     {
+        IsMove = false;
         transform.position = Vector3.MoveTowards(transform.position, moveSpot[randomSpot].position, speed * Time.deltaTime);
         Vector3 dir = moveSpot[randomSpot].position - transform.position;
         Quaternion targetAngle = Quaternion.LookRotation(dir);
@@ -119,8 +85,4 @@ public class Look : MonoBehaviour
     {
         randomSpot = Random.Range(0, moveSpot.Length);
     }
-    
-
-
-
 }
