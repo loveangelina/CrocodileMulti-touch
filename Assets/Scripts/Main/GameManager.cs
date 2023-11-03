b1d5bda9797f2aa34117efb0cf29388c3c65f097
loveangelina <90149613+loveangelina@users.noreply.github.com>
2023-11-03 16:01:26 +0900

    add SelectPunisher script
    벌칙자 선정 script인 SelectPunisher 추가, touchpointindex at GameManager

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -8,11 +8,18 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance => instance;
 
     private List<GameObject> touchpoints;
+    private int touchpointIndex;
 
     public List<GameObject> Touchpoints
     {
         get { return touchpoints; }
         set { touchpoints = value; }
+    }
+
+    public int TouchpointIndex
+    {
+        get { return touchpointIndex; }
+        set { touchpointIndex = value; }
     }
 
     private void Awake()
