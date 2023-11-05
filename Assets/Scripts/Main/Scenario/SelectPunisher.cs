a05d85da0fee82ca2a0008e6ebcd52815558ae05
loveangelina <90149613+loveangelina@users.noreply.github.com>
2023-11-05 21:07:29 +0900

    connect MoveCrocodile
    벌칙자 선정 후 10초간 돌아다니다가 악어가 벌칙자에게 이동하는 것을 컨트롤러에 연결

---
+++ Assets/Scripts/Main/Scenario/SelectPunisher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SelectPunisher : ScenarioBase
+{
+    public override void Enter(ScenarioController controller)
+    {
+        int numberOfParticipants = UIManager.Instance.value;
+        List<GameObject> touchpoints = GameManager.Instance.Touchpoints;
+
+        int randomIndex = Random.Range(0, numberOfParticipants);
+        GameManager.Instance.TouchpointIndex = randomIndex;
+        Debug.Log("벌칙자 인덱스 : " + GameManager.Instance.TouchpointIndex);
+
+        Vector3 selectedPosition = touchpoints[randomIndex].transform.position;
+        Debug.Log("선택된 위치 : " + selectedPosition);
+
+        controller.SetNextScenario();
+    }
+
+    public override void Execute(ScenarioController controller)
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
